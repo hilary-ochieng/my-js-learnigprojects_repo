22b2bd3e101970c0ed74af54aac9703586d338de
hilary-ochieng <omondihilary7@gmail.com>
2023-03-07 22:24:50 +0300

    Sum of natural numbers

---
+++ sum.js
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+// Programme to find sum of natural numbers from 1 - 100
+
+
+let sum = 0;
+
+for(let i = 1; i <= 100; i++){
+     sum += i;
+
+    console.log(`${sum} + ${i} = ${sum}`);//outputs all 105 resulst plus the working if inside {}and only final total sum 5050 if outside {}
+
+}
+
