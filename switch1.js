2910ba7c0a1af64f49c99fb83ea91868e6721f63
hilary-ochieng <omondihilary7@gmail.com>
2023-03-07 22:35:32 +0300

    Switch statement to name months

---
+++ switch1.js
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ //programme that assigns months of the year names (switch statements)
+
+let number = parseInt(prompt("Enter month number between 1 and 12: "));
+
+switch(number){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+     case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+     case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("June");
+        break;
+    case 8:
+        console.log("June");
+        break;
+    case 9:
+        console.log("June");
+        break;
+    case 10:
+        console.log("June");
+        break;
+    case 11:
+        console.log("June");
+        break;
+    case 12:
+        console.log("June");
+        break;
+
+   default:
+      console.log("Invalid Entry");
+}
+
